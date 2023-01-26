b1aafb92dab27c879e7de71f49672515db453db3
MoonLGH <MoonLMC1212@gmail.com>
2023-01-26 14:07:37 +0000

    h-2 and my brain went so smooth, wtf computer olympic without an ide to access

---
+++ jilid3/latihan4.js
@@ -0,0 +1,46 @@
+// // 1
+// let ah = 5
+
+// ah = abdu + 3
+// let ag = ah - 5
+
+// abdu = ah-3
+
+// console.log(abdu)
+// console.log(ah)
+// console.log(ah+abdu)
+
+// abdu = x+3
+// ag = x-5
+// 2x-2
+
+
+
+// // 2
+// bukuKem = 36
+// ongkos = (10/100)*bukuKem
+// keuntungan = 9
+
+// console.log(((bukuKem+ongkos+keuntungan)-bukuKem)/bukuKem)
+
+
+// // 3
+// terigu = 1.5
+// air = 4.5
+// sisa = 3/4
+
+// ygbutuh = sisa/terigu*air
+// console.log(ygbutuh)
+
+// 4
+
+// function factorialize(num) {
+//     if (num < 0) 
+//           return -1;
+//     else if (num == 0) 
+//         return 1;
+//     else {
+//         return (num * factorialize(num - 1));
+//     }
+//   }
+// console.log((factorialize(80)*factorialize(38))/(factorialize(77)*factorialize(40)))  
---
+++ refrensi.md
@@ -0,0 +1,3 @@
+Book : Olimpiade sains komputer jilid 1-3
+By bina prestasi insani
+Lembaga Olimpiade Pendidikan Indonesia
